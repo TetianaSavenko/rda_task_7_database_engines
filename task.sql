b50430f0b0765c5d899f016c7d689ddec4ade136
Tetiana Savenko <tanyasavenko5@gmail.com>
2026-06-02 17:28:08 +0300

    "changes5"

--- task.sql
+++ task.sql
@@ -1,14 +1,12 @@
 CREATE DATABASE ShopDB;
 USE ShopDB;
 
--- Create a table to store countries
 CREATE TABLE Countries (
     ID INT,
     Name VARCHAR(50),
     PRIMARY KEY (ID)
 ) ENGINE=InnoDB;
 
--- Create a table for caching GeoIP data (Columns: ID, IPRange, CountryID)
 CREATE TABLE GeoIPCache (
     ID INT,
     IPRange VARCHAR(50),
@@ -16,16 +14,14 @@ CREATE TABLE GeoIPCache (
     PRIMARY KEY (ID)
 ) ENGINE=MEMORY;
 
--- Create a table for storing product descriptions for different countries (Columns: ID, Description, ProductID, CountryID)
 CREATE TABLE ProductDescription (
     ID INT,
     Description TEXT,
     ProductID INT,
     CountryID INT,
     PRIMARY KEY (ID)
-) ENGINE=InnoDB;
+) ENGINE=MyISAM;
 
--- Create a table for storing logs (Columns: ID, Timestamp, Message)
 CREATE TABLE Logs (
     ID INT,
     Timestamp DATETIME,
@@ -33,7 +29,6 @@ CREATE TABLE Logs (
     PRIMARY KEY (ID)
 ) ENGINE=BLACKHOLE;
 
--- Create a table for storing reporting data in CSV format (Columns: Date, ProductName, Orders)
 CREATE TABLE ProductReporting (
     Date DATE NOT NULL,
     ProductName VARCHAR(100) NOT NULL,
